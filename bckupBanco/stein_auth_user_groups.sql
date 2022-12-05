357a8b00ce33c4e06b9a349bfe22d12d7ff35bbf
Vitor Garcia <vitorpso270@gmail.com>
2022-12-05 20:33:39 -0300

    modified:   bckupBanco/stein_auth_group.sql 	modified:   bckupBanco/stein_auth_group_permissions.sql 	modified:   bckupBanco/stein_auth_permission.sql 	modified:   bckupBanco/stein_auth_user.sql 	modified:   bckupBanco/stein_auth_user_groups.sql 	modified:   bckupBanco/stein_auth_user_user_permissions.sql 	modified:   bckupBanco/stein_authtoken_token.sql 	modified:   bckupBanco/stein_django_admin_log.sql 	modified:   bckupBanco/stein_django_content_type.sql 	modified:   bckupBanco/stein_django_migrations.sql 	modified:   bckupBanco/stein_django_session.sql 	modified:   bckupBanco/stein_equipe_cargo.sql 	modified:   bckupBanco/stein_equipe_funcionario.sql 	modified:   bckupBanco/stein_equipe_setor.sql 	modified:   bckupBanco/stein_estoque_produto.sql 	modified:   bckupBanco/stein_estoque_tipoproduto.sql 	modified:   bckupBanco/stein_mesas_comanda.sql 	modified:   bckupBanco/stein_mesas_comanda_produto.sql 	modified:   bckupBanco/stein_mesas_mesa.sql 	modified:   bckupBanco/stein_moduloemail_emailanonimo.sql 	modified:   bckupBanco/stein_restapi_usuario.sql

--- bckupBanco/stein_auth_user_groups.sql
+++ bckupBanco/stein_auth_user_groups.sql
@@ -31,7 +31,7 @@ CREATE TABLE `auth_user_groups` (
   KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
   CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
   CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `auth_user_groups` (
 
 LOCK TABLES `auth_user_groups` WRITE;
 /*!40000 ALTER TABLE `auth_user_groups` DISABLE KEYS */;
-INSERT INTO `auth_user_groups` VALUES (1,12,5),(2,12,7),(20,13,5),(4,14,8),(5,15,5),(6,15,7),(27,16,6),(19,17,6),(18,17,7),(14,18,6),(22,19,5),(21,19,7),(23,20,8),(25,21,5),(24,21,7);
+INSERT INTO `auth_user_groups` VALUES (29,7,5),(30,7,6),(31,7,7),(28,7,8),(1,12,5),(2,12,7),(20,13,5),(4,14,8),(5,15,5),(6,15,7),(27,16,6),(19,17,6),(18,17,7),(14,18,6),(22,19,5),(21,19,7),(23,20,8),(25,21,5),(24,21,7);
 /*!40000 ALTER TABLE `auth_user_groups` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-23 22:36:22
+-- Dump completed on 2022-12-05 20:32:53
